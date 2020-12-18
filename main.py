c75653688a5568f860e528748509bb8efce4c262
Eric <eric.amilhat@epitech.eu>
2020-12-18 15:22:23 +0100

    added set Todo and get details

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 
 app = Flask(__name__)
 
-todos = ["Faire les courses", "apprendre python"]
+todos = [
+    {
+        "id": 0,
+        "task": "Faire les courses",
+        "isDone": False,
+    },
+    {
+        "id": 1,
+        "task": "Apprendre le JS",
+        "isDone": False,
+    },
+]
+
+@app.route('/todo/', methods=['GET'])
+def get_todo_details():
+    id = int(request.args.get("id")) # check if number is correct before converting to int
+    print(f'Searching for todo with id {id}')
+    for todo in todos:
+        if todo["id"] == id:
+            return jsonify(todo)
+    abort(404)
+
+@app.route('/todo/', methods=['POST'])
+def check_todo():
+    id = int(request.args.get("id"))
+    isDone = request.args.get("id") # check if number is correct before converting to int
+    if (isDone == "false"):
+        isDone = False
+    else:
+        isDone = True
+    print(f'Setting todo with id {id} at {isDone}')
+
+    for todo in todos:
+        if todo["id"] == id:
+            todo["isDone"] = isDone
+            return jsonify(todo)
+    abort(404)
 
 
 @app.route('/')
@@ -14,12 +50,21 @@ def index():
 def get_todos():
     return jsonify(todos)
 
+
 @app.route('/todo/', methods=['POST'])
 def add_todo():
     todo = request.form.get('todo')
     print(f'Adding ${todo} to the list...')
-    todos.append(todo)
+    id = len(todos)
+    todos.append({
+        "id": id,
+        "task": todo,
+        "isDone": False
+    })
     return jsonify(todos)
+
+
+
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
